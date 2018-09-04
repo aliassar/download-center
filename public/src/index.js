6f117d41cbc2559d1f230b423f2b764d8aa1dc73
mahdi <hadiloomehdi@gmail.com>
2018-09-04 11:02:22 +0430

    for demo

---
+++ public/src/index.js
@@ -0,0 +1,5 @@
+import DragAndDrop from "./components/drag-and-drop/index.js";
+
+const element = document.querySelector("#drag-and-drop");
+const dragAndDrop = new DragAndDrop(element);
+dragAndDrop.run();
